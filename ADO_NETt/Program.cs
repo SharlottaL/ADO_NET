c330c6ba79c3faa4f3d335f133622fd691724d1f
Sharlotta <Sharlotta@Botan>
2025-09-16 13:25:02 +0300

    Add project files.

---
+++ ADO_NETt/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ADO_NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            Console.WriteLine(connectionString);
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = connectionString;
+            connection.Open();
+            string cmd = "SELECT * FROM Directors;";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                Console.Write(reader.GetName(i) + "\t");
+            }
+            Console.WriteLine();
+            while (reader.Read())
+            {
+                // Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
+                for (int i = 0; i < reader.FieldCount; i++)
+                    Console.Write(reader[i] + "\t\t");
+
+                    Console.WriteLine();
+                
+            }
+            reader.Close();
+            connection.Close();
+        }
+    }
+}
+
