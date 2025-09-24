9218630d4e839b5e56203ce4b11d6fa4362fc837
Sharlotta <Sharlotta@Botan>
2025-09-24 10:44:26 +0300

    Connector DONE

--- ADO_NETt/Program.cs
+++ ADO_NETt/Program.cs
@@ -1,137 +1,74 @@
 ﻿//#define SCALAR_CHECK
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
-
+using System.Configuration;
 namespace ADO_NET
 {
     internal class Program
     {
-        static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-        static SqlConnection connection;
-        static void Main(string[] args)
-        {
-            Console.WriteLine(connectionString);
-            connection = new SqlConnection();
-            connection.ConnectionString = connectionString;
-            //Select("SELECT * FROM Directors");
-            //Select("SELECT * FROM Movies");
-            Select("*", "Directors");
-            Select("movie_name,realise_date,first_name+last_name AS Режиссер", "Movies,Directors", "director=director_id");
-            Insert();
-            SelectPQ();
-#if SCALAR_CHECK
-            connection.Open();
-            string cmd = "SELECT COUNT(*) FROM Directors";
-            SqlCommand command = new SqlCommand(cmd, connection);
-            Console.WriteLine($"Количество режиссереов:\t{command.ExecuteScalar()}");
-
-            command.CommandText = "SELECT COUNT(*) FROM Movies";
-            Console.WriteLine($"Количество киношек:\t{command.ExecuteScalar()}");
-
-            command.CommandText = "SELECT last_name FROM Directors WHERE first_name=N'James'";
-            Console.WriteLine(command.ExecuteScalar());
-            connection.Close();
-            Console.WriteLine(Scalar("SELECT last_name FROM Directors WHERE first_name=N'James'"));
-            Console.WriteLine(Scalar("SELECT COUNT(*) FROM Movies"));
+   
+#if ALL_IN_MAIN_CLASS
+		static string connectionString = "";
+		static SqlConnection connection; 
 #endif
 
-        }
-        static void Select(string fields, string tables, string condition = "")
+		static void Main(string[] args)
         {
-            connection.Open();
-            // string cmd = "SELECT * FROM Directors;";
-            string cmd = $"SELECT {fields} FROM {tables}";
-            if (condition != "") cmd += $" WHERE {condition}";
-            cmd += ";";
-            SqlCommand command = new SqlCommand(cmd, connection);
-            SqlDataReader reader = command.ExecuteReader();
-            for (int i = 0; i < reader.FieldCount; i++)
-            {
-                Console.Write(reader.GetName(i) + "\t");
-            }
-            Console.WriteLine();
-            while (reader.Read())
-            {
-                // Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
-                for (int i = 0; i < reader.FieldCount; i++)
-                    Console.Write(reader[i] + "\t\t");
+#if ALL_IN_MAIN_CLASS
+			//0) Достаем строку подключения из App.config:
+			connectionString = ConfigurationManager.ConnectionStrings["Movies"].ConnectionString;
+			//1) Создаем подключение к Базе данных на Сервере:
+			Console.WriteLine(connectionString);
+			connection = new SqlConnection();
+			connection.ConnectionString = connectionString;
 
-                Console.WriteLine();
+			//Select("SELECT * FROM Directors");
+			//Select("SELECT * FROM Movies");
+			Select("*", "Directors");
+			Select("movie_name,release_date,first_name+last_name AS Режиссер", "Movies,Directors", "director=director_id"); 
+#endif
 
-            }
-            reader.Close();
-            connection.Close();
-        }
-        static object Scalar(string cmd)
-        {
-            connection.Open();
-            SqlCommand command = new SqlCommand(cmd, connection);
-            object obj = command.ExecuteScalar();
-            connection.Close();
-            return obj;
-        }
+#if SCALAR_CHECK
+			connection.Open();
+			string cmd = "SELECT COUNT(*) FROM Directors";
+			SqlCommand command = new SqlCommand(cmd, connection);
+			Console.WriteLine($"Количество режиссереов:\t{command.ExecuteScalar()}");
 
-        static void Insert()
-        {
-            Console.Write("Введите имя: ");
-            string first_name = Console.ReadLine();
+			command.CommandText = "SELECT COUNT(*) FROM Movies";
+			Console.WriteLine($"Количество киношек:\t{command.ExecuteScalar()}");
 
-            Console.Write("Введите фамилию: ");
-            string last_name = Console.ReadLine();
+			command.CommandText = "SELECT last_name FROM Directors WHERE first_name=N'James'";
+			Console.WriteLine(command.ExecuteScalar());
 
-            string cmd =
-$"INSERT Directors(director_id,first_name,last_name) VALUES ({Convert.ToInt32(Scalar("SELECT MAX(director_id) FROM Directors")) + 1},N'{first_name}',N'{last_name}');";
+			connection.Close();
 
-            SqlCommand command = new SqlCommand(cmd, connection);
+			Console.WriteLine(Scalar("SELECT last_name FROM Directors WHERE first_name=N'James'"));
+			Console.WriteLine(Scalar("SELECT COUNT(*) FROM Movies")); 
+#endif
+            //InsertDirector();
+            //InsertMovie();
 
-            connection.Open();
-            command.ExecuteNonQuery();
-            connection.Close();
+            //MovieConnector movie_connector =
+            //	new MovieConnector(ConfigurationManager.ConnectionStrings["Movies"].ConnectionString);
+            //movie_connector.SelectDirectors();
+            //movie_connector.SelectMovies();
+            //movie_connector.InsertDirector();
+            //movie_connector.InsertMovie();
+            //movie_connector.Select("*", "Movies,Directors", "director=director_id;DROP TABLE Actors");
+            Connector connector =
+                new Connector(ConfigurationManager.ConnectionStrings["Movies"].ConnectionString);
+            connector.SelectWithParameters("James", "Cameron");
 
-            Select("*", "Directors");
         }
 
-        static void SelectPQ()
-        {
-            Console.Write("Введите имя: ");
-            string first_name = Console.ReadLine();
-
-            Console.Write("Введите фамилию: ");
-            string last_name = Console.ReadLine();
-
-            string cmd =
-           "SELECT * FROM Directors WHERE First_name = @first_name AND Last_name = @last_name";
-
-            SqlCommand command = new SqlCommand(cmd, connection);
-            command.Parameters.AddWithValue("@first_name", first_name);
-            command.Parameters.AddWithValue("@last_name", last_name);
-
-            connection.Open();
-            SqlDataReader reader = command.ExecuteReader();
-            for (int i = 0; i < reader.FieldCount; i++)
-            {
-                Console.Write(reader.GetName(i) + "\t");
-            }
-            Console.WriteLine();
-            while (reader.Read())
-            {
-                // Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
-                for (int i = 0; i < reader.FieldCount; i++)
-                    Console.Write(reader[i] + "\t\t");
-
-                Console.WriteLine();
-
-            }
-            reader.Close();
-            connection.Close();
-
-            Select("*", "Directors");
-        }
     }
 }
+    
+
 
---
+++ Academy/MainForm.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Academy
+{
+
+        public partial class MainForm : Form
+        {
+            string connectionString = "Data Source=BOTAN\\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection connection;
+        Dictionary<string, int> d_groupsDirection;
+            public MainForm()
+            {
+                InitializeComponent();
+                connection = new SqlConnection(connectionString);
+            //LoadDirections();
+            //LoadGroups();
+            dataGridViewDirections.DataSource = Select("*", "Directions");
+            dataGridViewGroups.DataSource = Select
+                (
+                "group_id, group_name, direction", "Groups, Directions", "direction = direction_id"
+                );
+            //LoadDisciplines();
+            d_groupsDirection  = LoadDataToComboBox("*", "Directions");
+            comboBoxGroupsDirections.Items.AddRange(d_groupsDirection.Keys.ToArray());
+            comboBoxGroupsDirections.SelectedIndex = 0;
+            }
+        DataTable Select(string fields, string tables, string condition = "")
+        {
+            DataTable table = new DataTable();
+            string cmd = $"SELECT {fields} FROM {tables}";
+            if (!string.IsNullOrWhiteSpace(condition)) 
+                cmd += $" WHERE {condition}";
+            cmd += ";";
+
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            for (int i = 0; i < reader.FieldCount; i++)
+                table.Columns.Add(reader.GetName(i));
+
+            while(reader.Read())
+            {
+                DataRow row = table.NewRow();
+                for (int i = 0; i < reader.FieldCount; i++) row[i] = reader[i];
+                table.Rows.Add(row);
+            }
+            reader.Close();
+            connection.Close();
+            return table;
+        }
+        void LoadDirections()
+        {
+            string cmd = "SELECT * FROM Directions";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable table = new DataTable();
+            for (int i = 0; i < reader.FieldCount; i++)
+                table.Columns.Add(reader.GetName(i));
+
+            while (reader.Read())
+            {
+                DataRow row = table.NewRow();
+                for (int i = 0; i < reader.FieldCount; i++)
+                    row[i] = reader[i];
+
+                table.Rows.Add(row);
+            }
+            reader.Close();
+            connection.Close();
+            dataGridViewDirections.DataSource = table;
+        }
+        void LoadGroups()
+        {
+            string cmd =
+                @"SELECT
+                group_id AS N'ID', group_name AS N'Группа', COUNT(stud_id) AS N'Количество студентов: ',  direction_name AS N'Направление'
+                FROM Students
+                RIGHT   JOIN Groups ON([group] = group_id)
+                        JOIN Directions ON (direction = direction_id)
+                GROUP BY group_id, group_name, direction, direction_name;";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable table = new DataTable();
+            for (int i = 0; i < reader.FieldCount; i++)
+                table.Columns.Add(reader.GetName(i));
+
+            while (reader.Read())
+            {
+                DataRow row = table.NewRow();
+                for (int i = 0; i < reader.FieldCount; i++)
+                    row[i] = reader[i];
+
+                table.Rows.Add(row);
+            }
+            reader.Close();
+            connection.Close();
+            dataGridViewDirections.DataSource = table;
+        }
+        void LoadDisciplines()
+        {
+            string cmd =
+                @"SELECT
+                direction_id AS N'ID, direction_name AN N'Направление', COUNT(group_id) AS N'Количество групп'
+                FROM Groups
+                RIGHT JOIN Directions ON (direction=direction_id)
+                GROUP BY direction_id, direction_name;";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable table = new DataTable();
+            for (int i = 0; i < reader.FieldCount; i++)
+                table.Columns.Add(reader.GetName(i));
+
+            while (reader.Read())
+            {
+                DataRow row = table.NewRow();
+                for (int i = 0; i < reader.FieldCount; i++)
+                    row[i] = reader[i];
+
+                table.Rows.Add(row);
+            }
+            reader.Close();
+            connection.Close();
+            dataGridViewDirections.DataSource = table;
+        }
+
+       Dictionary<string, int> LoadDataToComboBox(string fields, string tables)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            dictionary.Add("Все", 0);
+            string cmd = $"SELECT {fields} FROM {tables}";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                //comboBoxGroupsDirections.Items.Add(reader[1]);
+                dictionary.Add(reader[1].ToString(), Convert.ToInt32(reader[0]));
+                    }
+            reader.Close();
+            connection.Close();
+            return dictionary;
+        }
+
+        private void comboBoxGroupsDirections_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string condition = "direction = direction_id";
+            if (comboBoxGroupsDirections.SelectedItem.ToString() != "Все")
+                condition += $" AND direction = {d_groupsDirection[comboBoxGroupsDirections.SelectedItem.ToString()]}";
+            dataGridViewGroups.DataSource = Select
+                (
+                "group_id, group_name, direction",
+                "Groups, Directions",
+                 condition
+              );
+        }
+    }
+    }
+
