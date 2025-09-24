6089446f1b4b142a1f26bba50d762eac10bea5d3
Sharlotta <Sharlotta@Botan>
2025-09-24 16:39:32 +0300

    Tables & Status bar DONE (need check)

--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Runtime.InteropServices;
+using System.Drawing.Printing;
 namespace Academy
 {
 
@@ -18,7 +19,23 @@ namespace Academy
             SqlConnection connection;
         Dictionary<string, int> d_groupsDirection;
 
+        Query[] queries = new Query[]
+        {
+            new Query("stud_id, FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name) AS N'Студент', group_name AS N'Группа', direction_name AS N'Направление'",
+                "Students, Groups, Directions",
+                "[group] = group_id AND direction = direction_id"
+                ), 
+            new Query
+            (
+              "group_id, group_name, direction_name",
+              "Groups, Directions",
+              "direction=direction_id"
+            ),
+            new Query ("*", "Directions" ),
+            new Query ("*", "Disciplines" ),
+            new Query ("*", "Teachers" )
 
+        };
 
         readonly string[] statusBarMassages = new string[]
         {
@@ -63,13 +80,19 @@ namespace Academy
             comboBoxGroupsDirections.Items.AddRange(d_groupsDirection.Keys.ToArray());
             comboBoxGroupsDirections.SelectedIndex = 0;
             tabControl.SelectedIndex = 1;
+            for (int i =0; i < tabControl.TabCount; i++)
+            {
+                (this.Controls.Find($"dataGridView{tabControl.TabPages[i].Name.Remove(0, "tabPage".Length)}", true)[0] as DataGridView)
+                    .RowsAdded 
+                    += new DataGridViewRowsAddedEventHandler(this.dataGridViewChanged);
+            }
         }
         void LoadTab(int i)
         {
             string tableName = tabControl.TabPages[i].Name.Remove(0, "tabPage".Length);
             DataGridView dataGridView = this.Controls.Find($"dataGridView{tableName}", true)[0] as DataGridView;
-            dataGridView.DataSource = Select("*", tableName);
-            toolStripStatusLabel1.Text = $"{statusBarMassages[i]}: {dataGridView.RowCount - 1}";
+            dataGridView.DataSource = Select(queries[i].Fields, queries[i].Tables, queries[i].Condition);
+          //  toolStripStatusLabel1.Text = $"{statusBarMassages[i]}: {dataGridView.RowCount - 1}";
         }
         void FillStatusBar(int i)
         {
@@ -212,6 +235,10 @@ namespace Academy
         private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
         {
             LoadTab((sender as TabControl).SelectedIndex);
+        }
+        private void dataGridViewChanged(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = $"{statusBarMassages[tabControl.SelectedIndex]}: {(sender as DataGridView).RowCount - 1}";
         }
     }
     }
